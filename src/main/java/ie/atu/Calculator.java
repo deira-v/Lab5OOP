5bcf5a776a998bd71f54995642fbf265857e44d1
DeiraValci-STUDENT <deiravalci@gmail.com>
2025-11-12 16:43:27 +0000

    Lab 5, New Lab

---
+++ src/main/java/ie/atu/Calculator.java
@@ -0,0 +1,14 @@
+package ie.atu;
+
+public class Calculator {
+
+    public int add(int firstNum, int secondNum)
+    {
+        return firstNum + secondNum;
+    }
+
+    public int sub(int firstNum, int secondNum)
+    {
+        return firstNum - secondNum;
+    }
+}
---
+++ src/test/java/ie/atu/CalculatorTest.java
@@ -0,0 +1,29 @@
+package ie.atu;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    Calculator calc;
+
+    @BeforeEach
+    public void setUp()
+    {
+        calc = new Calculator();
+    }
+
+    @Test
+    public void testAdd_Success()
+    {
+        assertEquals(20, calc.add(16, 4));
+    }
+
+    @Test
+    public void testSub_Success()
+    {
+        assertEquals(20, calc.sub(16, 4));
+    }
+
+}
